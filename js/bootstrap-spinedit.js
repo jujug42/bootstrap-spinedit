0ee09d1f092ab28030b40ee234e56e9069470916
Aaron Hamid <aaron.hamid@gmail.com>
2014-08-01 16:20:10 -0400

    normalize indentation

--- js/bootstrap-spinedit.js
+++ js/bootstrap-spinedit.js
@@ -1,193 +1,193 @@
 ﻿jQuery.fn.mousehold = function (f) {
-    var timeout = 100;
-    if (f && typeof f == 'function') {
-        var intervalId = 0;
-        var firstStep = false;
-        var clearMousehold = undefined;
-        return this.each(function () {
-            $(this).mousedown(function () {
-                firstStep = true;
-                var ctr = 0;
-                var t = this;
-                intervalId = setInterval(function () {
-                    ctr++;
-                    f.call(t, ctr);
-                    firstStep = false;
-                }, timeout);
-            });
+  var timeout = 100;
+  if (f && typeof f == 'function') {
+    var intervalId = 0;
+    var firstStep = false;
+    var clearMousehold = undefined;
+    return this.each(function () {
+      $(this).mousedown(function () {
+        firstStep = true;
+        var ctr = 0;
+        var t = this;
+        intervalId = setInterval(function () {
+          ctr++;
+          f.call(t, ctr);
+          firstStep = false;
+        }, timeout);
+      });
 
-            clearMousehold = function () {
-                clearInterval(intervalId);
-                if (firstStep) f.call(this, 1);
-                firstStep = false;
-            };
+      clearMousehold = function () {
+        clearInterval(intervalId);
+        if (firstStep) f.call(this, 1);
+        firstStep = false;
+      };
 
-            $(this).mouseout(clearMousehold);
-            $(this).mouseup(clearMousehold);
-        });
-    }
+      $(this).mouseout(clearMousehold);
+      $(this).mouseup(clearMousehold);
+    });
+  }
 };
 
 !function ($) {
 
-    var SpinEdit = function (element, options) {
-        this.element = $(element);
-        this.element.addClass("spinedit");
-        this.element.addClass("noSelect");
-        this.intervalId = undefined;
+  var SpinEdit = function (element, options) {
+    this.element = $(element);
+    this.element.addClass("spinedit");
+    this.element.addClass("noSelect");
+    this.intervalId = undefined;
 
-        var hasOptions = typeof options == 'object';
+    var hasOptions = typeof options == 'object';
 
-        this.minimum = $.fn.spinedit.defaults.minimum;
-        if (hasOptions && typeof options.minimum == 'number') {
-            this.setMinimum(options.minimum);
-        }
+    this.minimum = $.fn.spinedit.defaults.minimum;
+    if (hasOptions && typeof options.minimum == 'number') {
+      this.setMinimum(options.minimum);
+    }
 
-        this.maximum = $.fn.spinedit.defaults.maximum;
-        if (hasOptions && typeof options.maximum == 'number') {
-            this.setMaximum(options.maximum);
-        }
+    this.maximum = $.fn.spinedit.defaults.maximum;
+    if (hasOptions && typeof options.maximum == 'number') {
+      this.setMaximum(options.maximum);
+    }
 
-        this.numberOfDecimals = $.fn.spinedit.defaults.numberOfDecimals;
-        if (hasOptions && typeof options.numberOfDecimals == 'number') {
-            this.setNumberOfDecimals(options.numberOfDecimals);
-        }        
-		
-		var value = $.fn.spinedit.defaults.value;
-        if (hasOptions && typeof options.value == 'number') {
-            value = options.value;
-        } else {			
-			if (this.element.val()) {
-				var initialValue = parseFloat(this.element.val());
-				if (!isNaN(initialValue)) value = initialValue.toFixed(this.numberOfDecimals);				
-			}
-		}		
-        this.setValue(value);		
+    this.numberOfDecimals = $.fn.spinedit.defaults.numberOfDecimals;
+    if (hasOptions && typeof options.numberOfDecimals == 'number') {
+      this.setNumberOfDecimals(options.numberOfDecimals);
+    }
 
-        this.step = $.fn.spinedit.defaults.step;
-        if (hasOptions && typeof options.step == 'number') {
-            this.setStep(options.step);
-        }
+    var value = $.fn.spinedit.defaults.value;
+    if (hasOptions && typeof options.value == 'number') {
+      value = options.value;
+    } else {
+      if (this.element.val()) {
+        var initialValue = parseFloat(this.element.val());
+        if (!isNaN(initialValue)) value = initialValue.toFixed(this.numberOfDecimals);
+      }
+    }
+    this.setValue(value);
 
-        var templateText = $.fn.spinedit.defaults.template;
-        if (hasOptions && typeof options.template == 'string') {
-          templateText = options.template;
-        }
-        template = $(templateText);
-        this.element.after(template);
-	$(template).each(function (i,x) {
-            $(x).bind('selectstart click mousedown', function () { return false; });
-        });
+    this.step = $.fn.spinedit.defaults.step;
+    if (hasOptions && typeof options.step == 'number') {
+      this.setStep(options.step);
+    }
 
-        template.find('.glyphicon-chevron-up').mousehold($.proxy(this.increase, this));
-        template.find('.glyphicon-chevron-down').mousehold($.proxy(this.decrease, this));
-        this.element.on('keypress', $.proxy(this._keypress, this));
-        this.element.on('blur', $.proxy(this._checkConstraints, this));
-    };
+    var templateText = $.fn.spinedit.defaults.template;
+    if (hasOptions && typeof options.template == 'string') {
+      templateText = options.template;
+    }
+    template = $(templateText);
+    this.element.after(template);
+    $(template).each(function (i,x) {
+      $(x).bind('selectstart click mousedown', function () { return false; });
+    });
 
-    SpinEdit.prototype = {
-        constructor: SpinEdit,
+    template.find('.glyphicon-chevron-up').mousehold($.proxy(this.increase, this));
+    template.find('.glyphicon-chevron-down').mousehold($.proxy(this.decrease, this));
+    this.element.on('keypress', $.proxy(this._keypress, this));
+    this.element.on('blur', $.proxy(this._checkConstraints, this));
+  };
 
-        setMinimum: function (value) {
-            this.minimum = parseFloat(value);
-        },
+  SpinEdit.prototype = {
+    constructor: SpinEdit,
 
-        setMaximum: function (value) {
-            this.maximum = parseFloat(value);
-        },
+    setMinimum: function (value) {
+      this.minimum = parseFloat(value);
+    },
 
-        setStep: function (value) {
-            this.step = parseFloat(value);
-        },
+    setMaximum: function (value) {
+      this.maximum = parseFloat(value);
+    },
 
-        setNumberOfDecimals: function (value) {
-            this.numberOfDecimals = parseInt(value);
-        },
+    setStep: function (value) {
+      this.step = parseFloat(value);
+    },
 
-        setValue: function (value) {
-            value = parseFloat(value);
-            if (isNaN(value))
-                value = this.minimum;
-            if (this.value == value)
-                return;
-            if (value < this.minimum)
-                value = this.minimum;
-            if (value > this.maximum)
-                value = this.maximum;
-            this.value = value;
-            this.element.val(this.value.toFixed(this.numberOfDecimals));
-            this.element.change();
+    setNumberOfDecimals: function (value) {
+      this.numberOfDecimals = parseInt(value);
+    },
 
-            this.element.trigger({
-                type: "valueChanged",
-                value: parseFloat(this.value.toFixed(this.numberOfDecimals))
-            });
-        },
+    setValue: function (value) {
+      value = parseFloat(value);
+      if (isNaN(value))
+        value = this.minimum;
+      if (this.value == value)
+        return;
+      if (value < this.minimum)
+        value = this.minimum;
+      if (value > this.maximum)
+        value = this.maximum;
+      this.value = value;
+      this.element.val(this.value.toFixed(this.numberOfDecimals));
+      this.element.change();
 
-        increase: function () {
-            var newValue = this.value + this.step;
-            this.setValue(newValue);
-        },
+      this.element.trigger({
+        type: "valueChanged",
+        value: parseFloat(this.value.toFixed(this.numberOfDecimals))
+      });
+    },
 
-        decrease: function () {
-            var newValue = this.value - this.step;
-            this.setValue(newValue);
-        },
+    increase: function () {
+      var newValue = this.value + this.step;
+      this.setValue(newValue);
+    },
 
-        _keypress: function (event) {
-            var key = event.keyCode || event.charCode;
-            // Allow: -
-            if (key == 45) {
-                return;
-            }
-            // Allow decimal separator (.)
-            if (this.numberOfDecimals > 0 && key == 46) {
-                return;
-            }
-            // Ensure that it is a number and stop the keypress
-            var a = [];
-            for (var i = 48; i < 58; i++)
-                a.push(i);
-            if (!(a.indexOf(key) >= 0))
-                event.preventDefault();
-        },
+    decrease: function () {
+      var newValue = this.value - this.step;
+      this.setValue(newValue);
+    },
 
-        _checkConstraints: function (e) {
-            var target = $(e.target);
-            this.setValue(target.val());
-        }
-    };
+    _keypress: function (event) {
+      var key = event.keyCode || event.charCode;
+      // Allow: -
+      if (key == 45) {
+        return;
+      }
+      // Allow decimal separator (.)
+      if (this.numberOfDecimals > 0 && key == 46) {
+        return;
+      }
+      // Ensure that it is a number and stop the keypress
+      var a = [];
+      for (var i = 48; i < 58; i++)
+        a.push(i);
+      if (!(a.indexOf(key) >= 0))
+        event.preventDefault();
+    },
 
-    $.fn.spinedit = function (option) {
-        var args = Array.apply(null, arguments);
-        args.shift();
-        return this.each(function () {
-            var $this = $(this),
-				data = $this.data('spinedit'),
-				options = typeof option == 'object' && option;
+    _checkConstraints: function (e) {
+      var target = $(e.target);
+      this.setValue(target.val());
+    }
+  };
 
-            if (!data) {
-                $this.data('spinedit', new SpinEdit(this, $.extend({}, $.fn.spinedit().defaults, options)));
-				data = $this.data('spinedit');
-            }
-            if (typeof option == 'string' && typeof data[option] == 'function') {
-                data[option].apply(data, args);
-            }
-        });
-    };
+  $.fn.spinedit = function (option) {
+    var args = Array.apply(null, arguments);
+    args.shift();
+    return this.each(function () {
+      var $this = $(this),
+        data = $this.data('spinedit'),
+        options = typeof option == 'object' && option;
 
-    $.fn.spinedit.defaults = {
-        value: 0,
-        minimum: 0,
-        maximum: 100,
-        step: 1,
-        numberOfDecimals: 0,
-        template: '<span class="spinedit">' +
-          '<span class="glyphicon glyphicon-chevron-down"></span>' +
-          '<span class="glyphicon glyphicon-chevron-up"></span>' +
-        '</span>'
-    };
+      if (!data) {
+        $this.data('spinedit', new SpinEdit(this, $.extend({}, $.fn.spinedit().defaults, options)));
+        data = $this.data('spinedit');
+      }
+      if (typeof option == 'string' && typeof data[option] == 'function') {
+        data[option].apply(data, args);
+      }
+    });
+  };
 
-    $.fn.spinedit.Constructor = SpinEdit;
+  $.fn.spinedit.defaults = {
+    value: 0,
+    minimum: 0,
+    maximum: 100,
+    step: 1,
+    numberOfDecimals: 0,
+    template: '<span class="spinedit">' +
+      '<span class="glyphicon glyphicon-chevron-down"></span>' +
+      '<span class="glyphicon glyphicon-chevron-up"></span>' +
+      '</span>'
+  };
+
+  $.fn.spinedit.Constructor = SpinEdit;
 
 }(window.jQuery);
